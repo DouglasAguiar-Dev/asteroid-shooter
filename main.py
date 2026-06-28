56e62aa3fd65b4422f284ea5fdfc9a9880ddf904
DouglasAguiarDS <your@email.com>
2026-06-28 19:39:32 +0100

    Set up surface, color display and color surface

--- main.py
+++ main.py
@@ -10,7 +10,11 @@ WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
 # Get the display surface and shown on the screen with the specified width and height
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 
+# get the caption of the window and set it to "Asteroids Shooter"
 pygame.display.set_caption("Asteroids Shooter")
+
+# create a new surface with the specified width and height
+test_surf = pygame.Surface((400, 100)) 
 
 # keeps our game running
 while True:
@@ -21,6 +25,9 @@ while True:
             sys.exit() # the code will stop running after this line, so we need to call sys.exit() to exit the program
 
     #2 update game state 
+    display_surface.fill((215, 148, 45)) # fill the display surface with color of your choice
+    display_surface.blit(test_surf,(0,0)) # draw the test surface to the display surface at the specified position (0, 0)
+    test_surf.fill((0, 0, 255)) # fill the test surface with blue color
 
     #3 draw the game state to the display surface
-    pygame.display.update() # update the display surface
+    pygame.display.update() # update the display surface 
